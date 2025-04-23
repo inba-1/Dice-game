44e7b7f303b911e3e312f21d93bc66763120548e
inba-1 <inbakumar200@gmail.com>
2025-04-23 16:37:49 +0530

    dice 2 completed

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var dice1 = Math.floor(Math.random() * 6) + 1;
 var dice1img=document.querySelectorAll("img")[0].setAttribute("src", "/images/dice"+dice1+".png");
+var dice2=Math.floor(Math.random()*6)+1;
+var diceimg2=document.querySelectorAll("img")[1].setAttribute("src","/images/dice"+dice2+".png");
+if(dice1>dice2){
+    document.querySelector("h1").innerHTML("Player1 wins");
+}else if(dice2>dice1){
+    document.querySelector("h1").innerHTML("player2 wins");
+}else{
+    document.querySelector("h1").innerHTML("Draw !"); 
+}
