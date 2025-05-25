a0a85b1b6f9411cde998a6c19584c45dc02ec882
Inbakumar <143822939+inba-1@users.noreply.github.com>
2025-05-25 21:43:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var dice1 = Math.floor(Math.random() * 6) + 1;
-var dice1img=document.querySelectorAll("img")[0].setAttribute("src", "/images/dice"+dice1+".png");
+var dice1img=document.querySelectorAll("img")[0].setAttribute("src", "images/dice"+dice1+".png");
 var dice2=Math.floor(Math.random()*6)+1;
-var diceimg2=document.querySelectorAll("img")[1].setAttribute("src","/images/dice"+dice2+".png");
+var diceimg2=document.querySelectorAll("img")[1].setAttribute("src","images/dice"+dice2+".png");
 if (dice1 > dice2) {
     document.querySelector("h1").innerHTML = "🚩Player 1 wins";
 } else if (dice2 > dice1) {
